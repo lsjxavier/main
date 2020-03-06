51efdb350b688725b8d1a613a940dd00119ce562
lsjxavier <lsjxav95@gmail.com>
2020-03-06 22:49:16 +0800

    Add transactions command

--- src/main/java/seedu/foodiebot/logic/commands/TransactionsCommand.java
+++ src/main/java/seedu/foodiebot/logic/commands/TransactionsCommand.java
@@ -1,22 +1,43 @@
 package seedu.foodiebot.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.foodiebot.logic.parser.CliSyntax.PREFIX_FROM_DATE;
+import static seedu.foodiebot.logic.parser.CliSyntax.PREFIX_TO_DATE;
 
-import seedu.foodiebot.model.FoodieBot;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
 import seedu.foodiebot.model.Model;
 
 /** Gets the latest food transactions where reviews and single-user ratings can be added. */
 public class TransactionsCommand extends Command {
     public static final String COMMAND_WORD = "transactions";
 
-    public static final String MESSAGE_USAGE = "";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + " "
+            + PREFIX_FROM_DATE
+            + " FROM_DATE "
+            + PREFIX_TO_DATE
+            + " TO_DATE\n"
+            + "Example: "
+            + COMMAND_WORD
+            + " "
+            + PREFIX_FROM_DATE
+            + "14/2/2020 "
+            + PREFIX_TO_DATE + "24/2/2020";
 
-    public static final String MESSAGE_SUCCESS = "";
+    public static final String MESSAGE_SUCCESS = "Here are your expenses for ";
+
+    private final Stream<LocalDate> dateRange;
+
+    public TransactionsCommand(Stream<LocalDate> dateRange) {
+        this.dateRange = dateRange;
+    }
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setFoodieBot(new FoodieBot());
+
         return new CommandResult(COMMAND_WORD, MESSAGE_SUCCESS);
     }
 }
--- src/main/java/seedu/foodiebot/logic/parser/TransactionsCommandParser.java
+++ src/main/java/seedu/foodiebot/logic/parser/TransactionsCommandParser.java
@@ -1,12 +1,20 @@
 package seedu.foodiebot.logic.parser;
 
-import static seedu.foodiebot.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.foodiebot.commons.core.Messages.MESSAGE_INVALID_DATE;
+import static seedu.foodiebot.logic.parser.CliSyntax.PREFIX_DATE_BY_MONTH;
+import static seedu.foodiebot.logic.parser.CliSyntax.PREFIX_DATE_BY_YEAR;
+import static seedu.foodiebot.logic.parser.CliSyntax.PREFIX_FROM_DATE;
+import static seedu.foodiebot.logic.parser.CliSyntax.PREFIX_TO_DATE;
+
+import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import seedu.foodiebot.logic.commands.TransactionsCommand;
 import seedu.foodiebot.logic.parser.exceptions.ParseException;
 
 /** Parses input arguments and creates a new TransactionsCommand object */
 public class TransactionsCommandParser implements Parser<TransactionsCommand> {
+
     /**
      * Parses the given {@code String} of arguments in the context of the TransactionsCommand and returns a
      * TransactionsCommand object for execution.
@@ -14,14 +22,13 @@ public class TransactionsCommandParser implements Parser<TransactionsCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public TransactionsCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                String.format(MESSAGE_INVALID_COMMAND_FORMAT, TransactionsCommand.MESSAGE_USAGE));
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_FROM_DATE, PREFIX_TO_DATE,
+                PREFIX_DATE_BY_MONTH, PREFIX_DATE_BY_YEAR);
+        try {
+            Stream<LocalDate> dateRange = DateRangeFormatter.getDateRange(argMultimap);
+            return new TransactionsCommand(dateRange);
+        } catch (IllegalArgumentException iae) {
+            throw new ParseException(MESSAGE_INVALID_DATE);
         }
-
-        String[] nameKeywords = trimmedArgs.split("\\s+");
-
-        return new TransactionsCommand();
     }
 }
